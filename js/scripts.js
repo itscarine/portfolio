e80d8f1e83c2285fb40a851cc4cb2602fe4fcced
itscarine <89578551+itscarine@users.noreply.github.com>
2021-10-16 18:27:49 +0930

    add a hamburger for screens <641px

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log ("Hi there! Welcome to my portfolio website")
+
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
